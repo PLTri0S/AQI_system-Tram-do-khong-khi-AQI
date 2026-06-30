632e3985d6804636d02dd224da2038924a30ecf1
PLTri0S <tri.phungle@gmail.com>
2026-06-30 20:40:29 +0700

    Update log.h

--- AQI/log.h
+++ AQI/log.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 
 // Function to connect to your Wi-Fi network
-void setupWiFi(const char* ssid, const char* password);
+void setupWiFi();
 
 // Function to send the JSON string to Google Sheets
 void sendToGoogle(const String& scriptUrl, String jsonData);
